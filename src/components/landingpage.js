7316aee135415bc6e01a7eab910299213eef2d93
Saikrishnan <krishnansai99@gmail.com>
2020-08-02 23:10:43 +0530

    My Portfolio

---
+++ src/components/landingpage.js
@@ -0,0 +1,47 @@
+import React, { Component} from 'react';
+
+import { Grid, Cell } from 'react-mdl'  
+
+class Landing extends Component {
+    render() {
+        return(
+           <div style = {{width: '100%', margin: 'auto'}}>
+               <Grid className = "landing-grid">
+                   <Cell col = {12} >
+                   <img src = "https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                   alt = "avater"
+                   className = "avatar-img">
+                   </img>
+                   <div className="banner-text">
+                       <h1>Full Stack QA Engineer</h1>
+
+                       <hr />
+
+                       <p> HTML | Java | Javascript | Typescript | Rest API | Selenium | Appium | Protractor</p>
+
+                       <div className="social-links">
+
+                        {/* Linkedin */}
+                        <a href="https://www.linkedin.com/in/sri-sai-krishnan-v-21a212103/" rel="noopener noreferrer" target="_blank">
+                            <i className="fa fa-linkedin-square" aria-hidden="true" />
+                        </a>
+
+                        {/* Github */}
+                        <a href="https://github.com/SriSaiKrishnan" rel="noopener noreferrer" target="_blank">
+                            <i className="fa fa-github-square" aria-hidden="true" />
+                        </a>
+
+                         {/* Hacker Rank */}
+                         <a href="https://www.hackerrank.com/krishnansai99" rel="noopener noreferrer" target="_blank">
+                            <i className="fa fa-fire" aria-hidden="true" />
+                        </a>
+                       </div>
+                   </div>
+                   </Cell>
+               </Grid>
+           </div>
+        )
+    }
+}
+
+export default Landing;
---
+++ src/components/resume.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
+
+
+class Resume extends Component {
+  render() {
+    return(
+      <div>
+        <Grid>
+          <Cell col={4}>
+            <div style={{textAlign: 'center'}}>
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                alt="avatar"
+                style={{height: '200px'}}
+                 />
+            </div>
+
+            <h2 style={{paddingTop: '2em'}}>Sri Sai Krishnan V</h2>
+            <h4 style={{color: 'grey'}}>Software Engineer in Test</h4>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <p>I am an experienced Software Engineer in Test that includes Test Plan, Test Case Creation, Defect Management, STLC, Web-based testing, Mobility testing, Agile Methodologies, DevOps across different domains. Expertise in automating the Web & Mobile Application tests that include Selenium, JUnit, TestNG, Appium, Cucumber, Jenkins, Java, Concordian, Selenide, Protractor.
+            Experienced in the testing of Education, Healthcare, Retail Banking domains.</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h5>Address</h5>
+            <p>Selaiyur, Chennai</p>
+            <h5>Phone</h5>
+            <p>(+91) 8056200680</p>
+            <h5>Email</h5>
+            <p>krishnansai99@gmail.com</p>
+            <h5>Web</h5>
+            <p>mywebsite.com</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            <h2>Education</h2>
+
+
+            <Education
+              startYear={2011}
+              endYear={20115}
+              schoolName="Jeppiaar Engineering College"
+              schoolDescription="B.Tech Information Technology. CGPA - 7.5"
+               />
+
+               <Education
+                 startYear={2010}
+                 endYear={2011}
+                 schoolName="Higher Secondary School"
+                 schoolDescription="Computer Sciene Percentage - 90%"
+                  />
+                <hr style={{borderTop: '3px solid #e22947'}} />
+
+              <h2>Experience</h2>
+
+              <Experience
+                startYear={2019}
+                endYear = {2020}
+                jobName="Software Engineer in Test"
+                jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                />
+
+              <Experience
+                startYear={2018}
+                endYear={2019}
+                jobName="Test Engineer"
+                jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                />
+
+            <Experience
+              startYear={2015}
+              endYear={2018}
+              jobName="Quality Assurance Analyst"
+              jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+              />
+
+              <hr style={{borderTop: '3px solid #e22947'}} />
+              <h2>Skills</h2>
+                <Skills
+                skill="Java"
+                progress={100}
+                />
+                <Skills
+                  skill="Selenium"
+                  progress={100}
+                  />
+                  <Skills
+                    skill="Appium"
+                    progress={80}
+                    />
+                    <Skills
+                      skill="Rest API"
+                      progress={70}
+                      />
+
+
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Resume;
